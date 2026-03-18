c27a7839ee0c9a04a2c639cd6ac5f570b86d3091
Domingos da Costa Chilanda António <silanda@aluno.unilab.edu.br>
2026-03-18 00:13:26 -0300

    estutura base ddd

---
+++ src/Client/Client.java
@@ -0,0 +1,18 @@
+package Client;
+
+import java.util.UUID;
+
+public class Client {
+    private UUID id;
+    private String name;
+    private String document;
+    private String password;
+
+    public String getName(){
+        return this.name;
+    }
+    public void setName( String name){
+        this.name = name;
+    }
+
+}
